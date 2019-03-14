7ce9f27e1ec4227451894604a9c8c33b71cae65c
RouxRC <b.ooghe@gmail.com>
2019-03-14 02:36:47 +0100

    reorganise files

--- update.sh
+++ update.sh
@@ -6,11 +6,12 @@ dat=$(date +%y%m%d)
 
 function update_file {
   url=$1
-  fil=$2
+  room=$2
+  fil=$3
   mot=$(echo $fil | sed 's/ .*$//')
   echo "Download «$fil» at $url..."
   last=$(ls pdfs/*$mot* | tail -1)
-  new="pdfs/$dat - AN - $fil.pdf"
+  new="pdfs/$room/$fil - $dat.pdf"
   wget -q "$url" -O "$new"
   if test -s "$new" && diff "$last" "$new" | grep . > /dev/null; then
     echo "  -> New version!"
@@ -20,16 +21,16 @@ function update_file {
   echo
 }
 
-git pull > /tmp/load_reglement_budgetaire_an.tmp
-update_file "http://www2.assemblee-nationale.fr/static/deontologue/12_XV_bureau_frais_mandat_061218.pdf" "Arrêté Bureau - Frais de mandat 12 XV" >> /tmp/load_reglement_budgetaire_an.tmp
+git pull > /tmp/load_documents_parl.tmp
+update_file "http://www2.assemblee-nationale.fr/static/deontologue/12_XV_bureau_frais_mandat_061218.pdf" AN "Arrêté Bureau - Frais de mandat 12 XV" >> /tmp/load_documents_parl.tmp
 # http://www2.assemblee-nationale.fr/static/arrete_Bureau_12_XV_consolide.pdf
 # http://www2.assemblee-nationale.fr/static/12_XV_Bureau_frais%20de%20mandat.pdf
 # http://www2.assemblee-nationale.fr/static/arrete_bureau_12_XV_291117.pdf
-update_file "http://www2.assemblee-nationale.fr/static/comptes/RBCF.pdf" "Réglement budgétaire, comptable et financier" >> /tmp/load_reglement_budgetaire_an.tmp
+update_file "http://www2.assemblee-nationale.fr/static/comptes/RBCF.pdf" AN "Réglement budgétaire, comptable et financier" >> /tmp/load_documents_parl.tmp
 
 if git status | grep "pdfs" > /dev/null; then
-  cat /tmp/load_reglement_budgetaire_an.tmp
-  git add pdfs/$dat*
-  git commit -m "Update documents budgétaires et financiers AN - 20$dat"
+  cat /tmp/load_documents_parl.tmp
+  git add pdfs/*/*$dat.pdf
+  git commit -m "Update documents parlementaires - 20$dat"
   git push
 fi
